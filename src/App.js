06482f1cb9d90a3c3401f12dc381f7281accacce
binhdv <duongvanbinh3111997@gmail.com>
2020-06-12 00:09:03 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ColerPicker from './components/ColorPicker';
+import Sizesetting from './components/Sizesetting';
+import Reset from './components/Reset';
+import Result from './components/Result';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color : 'red',
+            fontSize: 15
+        }
+        this.onSetColor = this.onSetColor.bind(this);
+        this.onChangeSize = this.onChangeSize.bind(this);
+        this.onSettingdefault = this.onSettingdefault.bind(this);
+    }
+    onSetColor(params){
+        console.log(params);
+        this.setState ({
+            color : params /// neu de the nay thi no khong nhan minh phai bind no trong constructor nhe
+        });
+    }
+    onChangeSize (value){
+       // console.log(value);
+       if(this.state.fontSize + value >8 && this.state.fontSize + value <38){
+           this.setState({
+               fontSize: this.state.fontSize + value 
+           });
+       }
+    }
+    onSettingdefault(params){
+        console.log(params);
+        if (params ){
+            this.setState({
+                color : 'red',
+                fontSize: 15
+            });
+        }
+    }
+    render(){
+        return (
+            <div className="container mt-50">
+                <div className="row">
+                    <ColerPicker color = {this.state.color} onReceiveColor= {this.onSetColor}/>
+                    <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <Sizesetting fontSize = {this.state.fontSize} onChangeSize={this.onChangeSize}/>
+                        <Reset onSettingdefault={this.onSettingdefault}/>
+                    </div>
+                    <Result color = {this.state.color} fontSize = {this.state.fontSize}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+    setStyle(){
+        return {
+            color : this.props.color,
+            borderColor : this.props.color,
+            fontSize : this.props.fontSize
+        }
+    }
+    render(){
+        return (
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <p>color :  {this.props.color} - fontsize: {this.props.fontSize}</p>
+                <div id="content" style = {this.setStyle()}>
+                    noi dung setting
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Result;
